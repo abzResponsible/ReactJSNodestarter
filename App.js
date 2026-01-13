04ae5885efe5073c82a5d8196262dd8f5292f794
Abdi Abyan <aa7872@ou.ac.uk>
2026-01-13 12:25:48 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,28 @@
+import React from 'react';
+import Styles'./styles.css';
+import {Router, Switch, Redirect} from 
+'react-router-dom';
+import Home from './home.jsx';
+import Contact from './Contect.jsx';
+import About from './about.jsx';
+import Services from './Services.jsx';
+import Navbar from './Navbar.jsx';
+import Navbar from './Navbar.js';
+
+const App = () => {
+return (
+<>
+  <Switch> 
+  <Router exact path="/" component={Home} />
+  <Router exact path="/contact" component={Contact} />
+  <Router exact path="/about" component={About} />
+  <Router exact path="/services" component={Services} />
+  <Redirect to "/"; />
+  </Switch> 
+  </>
+);
+}
+
+export defualt App;
+
 
